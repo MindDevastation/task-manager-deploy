408f705d97bfcd2b966d6313f92aa62f7edf58e2
Aleksey Pastukhov <mindflux99@gmail.com>
2025-02-21 10:09:35 +0200

    Save changes before checkout

--- core/settings/base.py
+++ core/settings/base.py
@@ -126,3 +126,23 @@ LOGIN_REDIRECT_URL = "/"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 AUTH_USER_MODEL = "home.Worker"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "ERROR",
+            "class": "logging.FileHandler",
+            "filename": "error.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
+
